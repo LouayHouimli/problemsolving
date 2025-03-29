59192a39df172cb7871cd2a8aa1ee04dc50a3ba0
LouayHouimli <louayhouimli@engineer.com>
2025-03-29 13:18:59 +0100

    add palindrome.ts

---
+++ leetcode/palindrome/palindfrome.ts
@@ -0,0 +1,6 @@
+function isPalindrome(x: number): boolean {
+  if (x < 0) return false;
+  const str = x.toString();
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
